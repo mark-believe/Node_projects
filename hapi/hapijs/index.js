fef900b696a23e9c736956e8f504bb9de2bdf353
mark-believe <love123zst@163.com>
2019-06-11 14:39:19 +0800

    hapijs

---
+++ hapi/hapijs/index.js
@@ -0,0 +1,39 @@
+"use strict"
+
+const Hapi = require('hapi');
+
+const server = Hapi.server({
+    port:3000,
+    host:'localhost'
+});
+
+const init = async () => {
+
+    server.route({
+        path: '/',
+        method: 'GET',
+        handler () {
+            return 'Hapi world'
+        }
+    });
+
+    server.route({
+        path: '/api/welcome',
+        method: 'GET',
+        handler (request) {
+            return {
+                code: 200,
+                success: true,
+                
+                data: {
+                    msg: 'welcome ${request.query.name}'
+                }
+            }
+        }
+    });
+    
+    await server.start();
+    console.log('Server running at: ${server.info.uri}');
+}
+
+init();
