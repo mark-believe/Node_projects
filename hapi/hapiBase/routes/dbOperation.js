ed45129080cd0f518c7cc567af3092458fdb05bb
mark-believe <love123zst@163.com>
2019-06-12 18:33:36 +0800

    CURD test for DB

--- hapi/hapiBase/routes/dbOperation.js
+++ hapi/hapiBase/routes/dbOperation.js
@@ -13,6 +13,7 @@ function findFromDB(reply)
           throw err;
         }
         db.close();
+        console.log("文档查询成功");
         reply(result);
     });
   });
@@ -35,16 +36,71 @@ function insertToDB(reply)
         throw err;
       }
       console.log("插入的文档数量为: " + res.insertedCount);
-      db.close();
+      //db.close();
       reply("插入成功！")
     });
-});
+  });
+}
+
+function updateToDB(reply)
+{
+  db.MongoClient.connect(config.db, { useNewUrlParser: true }, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("test");
+    //var whereStr = {"name":'菜鸟工具'};  // 查询条件
+    var whereStr = {"name":{$regex:/菜鸟工具/}};
+    var updateStr = {$set: { "name" : "菜鸟工具-已修改" + Date.now() }};
+    dbo.collection("test").updateMany(whereStr, updateStr, function(err, res) {
+        if (err) throw err;
+        console.log("文档更新成功");
+        //db.close();
+        reply("更新成功")
+    });
+  });
+}
+
+function deleteFromDB(reply)
+{
+  db.MongoClient.connect(config.db, { useNewUrlParser: true }, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("test");
+    //var whereStr = {"name":'菜鸟工具'};  // 查询条件
+    var whereStr = {"name":{$regex:/菜鸟工具/}};
+    dbo.collection("test").deleteMany(whereStr, function(err, res) {
+        if (err) throw err;
+        console.log("文档删除成功");
+        //db.close();
+        reply("删除成功")
+    });
+  });
 }
 
 module.exports = [
     {
       method: 'GET',
+      path: '/db/c',
+      handler: (request, reply)=>{
+        insertToDB(reply);
+      },
+      config: {
+        tags: ['api','DB'],
+        description: '添加数据'
+      }
+    },
+    {
+      method: 'GET',
       path: '/db/u',
+      handler: (request, reply)=>{
+        updateToDB(reply);
+      },
+      config: {
+        tags: ['api','DB'],
+        description: '更新数据'
+      }
+    },
+    {
+      method: 'GET',
+      path: '/db/r',
       handler: (request, reply)=>{
         findFromDB(reply);
       },
@@ -55,13 +111,13 @@ module.exports = [
     },
     {
       method: 'GET',
-      path: '/db/c',
+      path: '/db/d',
       handler: (request, reply)=>{
-        insertToDB(reply);
+        deleteFromDB(reply);
       },
       config: {
         tags: ['api','DB'],
-        description: '添加数据'
+        description: '删除数据'
       }
     }
   ]
