70d351fc4c77f8010eba4fe31d7a2cacad4d212e
mark-believe <love123zst@163.com>
2019-06-21 18:09:16 +0800

    swagger 完善

--- hapi/official-test/routes.js
+++ hapi/official-test/routes.js
@@ -4,7 +4,8 @@ const Pages = require('./handlers/pages');
 const Assets = require('./handlers/assets');
 const Actions = require('./handlers/actions')
 const Authentication  = require('./handlers/authentication');
-        
+const Joi = require('joi');
+
 module.exports = [
     {
         method: ['GET', 'POST'], 
@@ -12,38 +13,69 @@ module.exports = [
         options: { 
             handler: Authentication.login, 
             auth: { mode: 'try' }, 
-            plugins: { 'hapi-auth-cookie': { redirectTo: false } } 
+            plugins: { 'hapi-auth-cookie': { redirectTo: false } } ,
+            // validate:{
+            //     params: {
+            //         username: Joi.string().required().description('用户名'),
+            //         password: Joi.string().required().description('密码')
+            //     }
+            // }
         } 
     },
     {
         method: 'GET', 
         path: '/logout', 
-        handler: Authentication.logout   
+        handler: Authentication.logout,
+        config: {
+            tags: ['Authentication'],
+            description: '退出'
+        }
+           
     },
     {
         method: 'GET',
         path: '/',
-        handler: Pages.home
+        handler: Pages.home,
+        config: {
+            tags: ['view'],
+            description: '主页'
+        }
     },
     {
         method: 'GET',
         path: '/recipes/{id}',
-        handler: Pages.viewRecipe
+        handler: Pages.viewRecipe,
+        config: {
+            tags: ['operation'],
+            description: '获取菜谱'
+        }
     },
     {
         method: 'GET',
         path: '/create',
-        handler: Pages.createRecipe
+        handler: Pages.createRecipe,
+        config: {
+            tags: ['operation'],
+            description: '获取菜谱'
+        }
     },
     {
         method: 'POST',
         path: '/create',
-        handler: Actions.createRecipe
+        handler: Actions.createRecipe,
+        config: {
+            tags: ['operation'],
+            description: '获取菜谱'
+        }
     },
     {
         method: 'GET',
         path: '/hello',
-        handler: Pages.hello
+        handler: Pages.hello,
+        config: {
+            tags: ['test'],
+            description: '测试'
+        }
     },  
     {
         method: 'GET',
